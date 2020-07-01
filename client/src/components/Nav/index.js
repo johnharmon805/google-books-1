69c69fbe3c2a9c6845c1d61d04960e7b093e2cca
John Harmon <johnharmon805@gmail.com>
2020-06-30 21:38:17 -0700

    2nd attempt at hw

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav
+    className="navbar navbar-expand-lg navbar-dark bg-primary" id="navbar">
+      <a className="navbar-brand" href="/">
+        Google Book Search
+      </a>
+    </nav>
+  );
+}
+
+export default Nav;
